32362caaea2b068b5db0a254949ccdbdda502e75
dhruv2253 <dhruv225@outlook.com>
2024-10-02 16:51:49 -0400

    Add registration confirm page

---
+++ src/app/register/confirm/page.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-center">
+          <p className="text-3xl font-bold">Thank you for registering <br /></p>
+        We have sent you a confirmation email. Please confirm your email address to activate your account.
+      </div>
+    </div>
+  );
+}
