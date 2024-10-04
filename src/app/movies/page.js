b77c2ac8e6262b481f06d95e9264a1fdd18422bf
Vansh Arora <vansharora@Vanshs-MacBook-Pro.local>
2024-10-04 18:15:16 -0400

    Search

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,44 +1,10 @@
 import MovieCard from "@/components/MovieCard";
 import MovieCarousel from "@/components/MovieCarousel";
+import movies from "../../resources/movies.json"
 
-const movie1 = {
-    id: 1,
-    title: "Dhoom 2",
-    trailer: "https://www.youtube.com/embed/oRIIeyIa6nE?si=2x9VP1D0Hn5UE-33"
-};
 
-const movie2 = {
-    id: 2,
-    title: "Inception",
-    trailer: "https://www.youtube.com/embed/YoHD9XEInc0"
-};
 
-const movie3 = {
-    id: 3,
-    title: "The Dark Knight",
-    trailer: "https://www.youtube.com/embed/EXeTwQWrcwY"
-};
 
-const movie4 = {
-    id: 4,
-    title: "Interstellar",
-    trailer: "https://www.youtube.com/embed/zSWdZVtXT7E"
-};
-
-const movie5 = {
-    id: 5,
-    title: "The Shawshank Redemption",
-    trailer: "https://www.youtube.com/embed/6hB3S9bIaco"
-};
-
-const movie6 = {
-    id: 6,
-    title: "Parasite",
-    trailer: "https://www.youtube.com/embed/5xH0HfJHsaY?si=2vsYfzxtr6_dc3g8"
-};
-
-// Array of movies
-const movies = [movie1, movie2, movie3, movie4, movie5, movie6];
 
 
 export default function Movies() {
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,28 @@
 "use client";
 /** This component renders the NavBar
  **/
+import { useEffect, useState } from "react";
+import movies from "../resources/movies.json"
+
 export default function NavBar() {
     const handleSearchClick = () => {
         const searchField = document.querySelector(".searchField")
+        const dropdown = document.querySelector(".moviesDropdown")
         searchField.style.display = searchField.style.display === 'none' ? 'block' : 'none';
+        dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none';
+
     }
+    const [searchInput, setSearchInput] = useState("")
+    const [displayedMovies, setDisplayedMovies] = useState([])
+
+
+    useEffect(() => {
+        if (searchInput === "") {
+            setDisplayedMovies([])
+        } else {
+            setDisplayedMovies(movies.filter(movie => movie.title.toLowerCase().includes(searchInput.toLowerCase())))
+        }
+    }, [searchInput])
     return (
     <div className="navbar">
         <div className="buttonRow">
@@ -13,7 +30,10 @@ export default function NavBar() {
         <a className="navbutton" onClick={handleSearchClick}>Search</a>
         <a className="navbutton" href="/login">Login</a>
         </div>
-        <input type="text" className="searchField" placeholder="Begin typing movie title..."></input>
+        <input type="text" className="searchField" placeholder="Begin typing movie title..." onChange={e => setSearchInput(e.target.value)}></input>
+        <div className="moviesDropdown">
+            {displayedMovies.map(m => <a href={`/movies/${m.id}`}><div className="moviesDropdownItem">{m.title}</div></a>)}
+        </div>
     </div>
     )
 }
