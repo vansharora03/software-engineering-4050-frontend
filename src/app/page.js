324b7e2bcf3149361eef16042fb5ee68b88b13d2
Vansh Arora <vansharora@Vanshs-MacBook-Pro.local>
2024-10-04 17:32:03 -0400

    Added movies page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,5 @@ import Image from "next/image";
 import NavBar from "../components/NavBar"
 
 export default function Home() {
-  return ( <NavBar></NavBar>
-  );
+  
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,7 +3,9 @@ export default function MovieCard({movie}) {
         <div className="movieCard" id={`movie-${movie.id}`}>
             <h2 className="movieCardTitle" id={`movie-${movie.id}-title`}>{movie.title}</h2>
             <iframe className="movieCardtTrailer" id={`movie-${movie.id}-video`} src={movie.trailer}></iframe>
-            <button className="movieCardBookButton">Book</button>
+            <a href={`/movies/${movie.id}`} id={`movie-${movie.id}-link`}>
+            <button className="movieCardBookButton">View</button>
+            </a>
         </div>
     )
 }
