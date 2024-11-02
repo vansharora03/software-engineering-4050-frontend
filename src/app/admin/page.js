a38696f5f48e9290f6de72cf5555f75e244bed8e
AbhiPorika <abhinavsporika@gmail.com>
2024-11-01 23:09:50 -0400

    Resolved Merge Conflicts

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function AdminMainScreen() {
     const router = useRouter();
